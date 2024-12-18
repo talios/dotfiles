9b5d7ab201044764c18cf0a9fb738e688d307e3a
Mark Derricutt <mark.derricutt@smxemail.com>
2024-12-19 10:10:03 +1300

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,7 +1,7 @@
 # Custom
-source $HOME/Dropbox/oh-my-fish/custom/talios.load
+source $HOME/.config/fish/talios.load
 
 # opam configuration
-source /Users/amrk/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
-set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+source /Users/amrk/.opam/opam-init/init.fish >/dev/null 2>/dev/null; or true
+set -g fish_user_paths /usr/local/sbin $fish_user_paths
 export RUBY_CONFIGURE_OPTS="--with-openssl-dir=(brew --prefix openssl@1.1)"
