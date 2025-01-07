000c286384a39ddcf9215f51f9f25743dcb3ea36
Mark Derricutt <mark.derricutt@smxemail.com>
2025-01-07 17:01:11 +1300

    Update .gitconfig Update .config/ghostty/config Update .config/fish/functions/remux-files.fish

--- private_dot_config/private_fish/functions/remux-files.fish
+++ private_dot_config/private_fish/functions/remux-files.fish
@@ -1,7 +1,9 @@
 function remux-files
-  for show in $argv
-    for i in *$show*264*
-      pueue add -g remux ~/go/bin/go265 --tmp /Volumes/crux/x265/ --path ./ --input \"$i\"
+    mkdir -p /tmp/x265
+    for show in $argv
+        for i in *$show*264*
+            # pueue add -g remux ~/go/bin/go265 --tmp /Volumes/crux/x265/ --path ./ --input \"$i\"
+            pueue add -g remux ~/go/bin/go265 --tmp /tmp/x265/ --path ./ --input \"$i\"
+        end
     end
-  end
 end
