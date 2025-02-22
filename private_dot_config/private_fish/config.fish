90a49dcd581892af649de68bee761ac798fe7127
Mark Derricutt <mark.derricutt@smxemail.com>
2025-02-22 13:59:06 +1300

    Update .netrc Update .config/ghostty/config Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -5,3 +5,7 @@ source $HOME/.config/fish/talios.load
 source /Users/amrk/.opam/opam-init/init.fish >/dev/null 2>/dev/null; or true
 set -g fish_user_paths /usr/local/sbin $fish_user_paths
 export RUBY_CONFIGURE_OPTS="--with-openssl-dir=(brew --prefix openssl@1.1)"
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init.fish 2>/dev/null || :
