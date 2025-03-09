c0b23a90c0052c11017654ca33aabcaff0d7f0ca
Mark Derricutt <mark.derricutt@smxemail.com>
2025-03-09 22:57:58 +1300

    Update Library/Application Support/jj/config.toml Update .config/ghostty/config Update .config/fish/functions/tidal-dir-fix.fish

--- private_dot_config/private_fish/functions/tidal-dir-fix.fish
+++ private_dot_config/private_fish/functions/tidal-dir-fix.fish
@@ -1,14 +1,13 @@
-
 function tidal-dir-fix
-  cd "/Volumes/Media Content-1/Plex Content/Music/Tidal Rips/Album"
-  for prefix in $argv
-    echo "• Fixing tidal album directory names for $prefix*"
-    set artists (command ls $prefix*)
-    for i in "$artists*"
-      echo "• Fixing tidal album directory names for artist $i*"
-      # cd "$i"
-      # rename-tidal-dir -s . --dry-run
-      # cd ..
+    cd "/Volumes/Media Content/Plex Content/Music/Tidal Rips/Album"
+    for prefix in $argv
+        echo "• Fixing tidal album directory names for $prefix*"
+        set artists (command ls $prefix*)
+        for i in "$artists*"
+            echo "• Fixing tidal album directory names for artist $i"
+            #            cd "$i"
+            #            rename-tidal-dir -s . --dry-run
+            #            cd ..
+        end
     end
-  end
 end
