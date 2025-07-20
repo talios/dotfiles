1a6523ccc4d0f1aa267ba01c23144753c389b85e
Mark Derricutt <mark.derricutt@smxemail.com>
2025-07-20 21:53:52 +1200

    Update .tigrc Update .config/ghostty/config Update .config/helix/config.toml Update .config/fish/config.fish Update .config/fish/talios.load Update .config/jj/config.toml Update .config/zed/settings.json

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -9,3 +9,6 @@ export RUBY_CONFIGURE_OPTS="--with-openssl-dir=(brew --prefix openssl@1.1)"
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init.fish 2>/dev/null || :
+
+# uv
+fish_add_path "/Users/amrk/.local/bin"
