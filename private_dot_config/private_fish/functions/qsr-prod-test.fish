437572e001982a88e04585ee90456e4cbbe281f1
Mark Derricutt <mark.derricutt@smxemail.com>
2025-07-30 15:51:54 +1200

    Update .gitconfig Update .config/ghostty/config Update .config/fish/functions/qsr-prod-test.fish Update .config/fish/talios.load Update .config/jj/config.toml Update .config/zed/settings.json

--- private_dot_config/private_fish/functions/qsr-prod-test.fish
+++ private_dot_config/private_fish/functions/qsr-prod-test.fish
@@ -1,9 +1,12 @@
 function qsr-prod-test --description "qsr-prod-test <count> <description>"
   set count $argv[1]
   set desc $argv[2..-1]
-  set PASSWORD (op item get "Calibre Gmail SMTP" --fields=password)
+  set PASSWORD (op --account HCGPZX5USVFSLJVQPCM6HLETMU item get "Calibre Gmail SMTP" --fields=password --reveal)
 
   for i in (seq 1 $count)
-    swaks --from mark@talios.com --to mark.derricutt@o365filter.com --server smtp.gmail.com --auth-user mark@talios.com --auth-password $PASSWORD --header-Subject "$i: $desc" -tls --body "$i: $desc"
+    swaks --from mark@talios.com --to mark.derricutt@o365filter.com \
+        --server smtp.gmail.com:587 -a PLAIN --auth-user mark@talios.com \
+        --auth-password $PASSWORD \
+        --header-Subject "$i: $desc" -tls --body "$i: $desc"
   end
 end
