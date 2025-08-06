9cbf95439cdf30777836dda5cc417402b7774170
Mark Derricutt <mark.derricutt@smxemail.com>
2025-08-06 19:26:16 +1200

    Update .aerospace.toml Update .config/ghostty/config Update .config/fish/functions/qsr-prod-test.fish Update .config/zed/settings.json

--- private_dot_config/private_fish/functions/qsr-prod-test.fish
+++ private_dot_config/private_fish/functions/qsr-prod-test.fish
@@ -4,9 +4,11 @@ function qsr-prod-test --description "qsr-prod-test <count> <description>"
   set PASSWORD (op --account HCGPZX5USVFSLJVQPCM6HLETMU item get "Calibre Gmail SMTP" --fields=password --reveal)
 
   for i in (seq 1 $count)
-    swaks --from mark@talios.com --to mark.derricutt@o365filter.com \
+    swaks --from mark@talios.com \
+        --to markd@o365filter.com \
         --server smtp.gmail.com:587 -a PLAIN --auth-user mark@talios.com \
         --auth-password $PASSWORD \
+        --header-From "Mark <mark@talios.com>" \
         --header-Subject "$i: $desc" -tls --body "$i: $desc"
   end
 end
