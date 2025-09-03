ce23e1c32289fdbc68a1c0e45c9e9615c76046dc
Mark Derricutt <mark.derricutt@smxemail.com>
2025-09-03 20:14:11 +1200

    Update .aws/config Update .config/ghostty/config Update .config/fish/functions/mvn-resolve-locked.fish Update .config/fish/functions/mvn-resolve.fish Update .config/fish/functions/qsr-test.fish Update .config/fish/talios.load Update .config/jj/config.toml

--- private_dot_config/private_fish/functions/mvn-resolve.fish
+++ private_dot_config/private_fish/functions/mvn-resolve.fish
@@ -1,5 +1,5 @@
 function mvn-resolve
-    set RANGERESOLVER 1.2.25
+    set RANGERESOLVER 1.2.28
     if test -f pom.deps
         if test -f release.properties
             echo "Found release.properties, aborting..."
