936d41b91efcbfe1d38197c7f90b8936c867dff6
Mark Derricutt <mark.derricutt@smxemail.com>
2025-09-29 22:59:29 +1300

    Update .config/fish/functions/git-commit-deps.fish Update .config/jj/config.toml

--- private_dot_config/private_fish/functions/git-commit-deps.fish
+++ private_dot_config/private_fish/functions/git-commit-deps.fish
@@ -5,12 +5,12 @@ function git-commit-deps
         else
             mvn-resolve
             git clean -fd
-            echo "build(deps): Synced bill-of-materials" >COMMIT_MSG.txt
-            echo "" >>COMMIT_MSG.txt
-            grep resolved pom.deps >>COMMIT_MSG.txt
+            echo "build(deps): Synced bill-of-materials" >/tmp/COMMIT_MSG.txt
+            echo "" >>/tmp/COMMIT_MSG.txt
+            grep resolved pom.deps >>/tmp/COMMIT_MSG.txt
             mvn com.smxemail:rangeresolver-maven-plugin:lock-deps
-            git commit -F COMMIT_MSG.txt (fd "pom.*") (fd "tile.xml")
-            rm COMMIT_MSG.txt
+            git commit -F /tmp/COMMIT_MSG.txt (fd "pom.*") (fd "tile.xml")
+            rm /tmp/COMMIT_MSG.txt
         end
     else
         echo "Missing pom.deps file in working directory.."
