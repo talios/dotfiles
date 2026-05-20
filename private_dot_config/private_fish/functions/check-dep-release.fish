404fc45cbeee59333f71d8746f74e3ac6c142d09
Mark Derricutt <mark.derricutt@smxemail.com>
2026-05-20 20:12:38 +1200

    Update .envrc Update .m2/settings.xml Update .config/omniwm/settings.toml Update .config/fish/functions/check-dep-release.fish Update .config/jj/config.toml Update .config/tidal_dl_ng/settings.json

--- private_dot_config/private_fish/functions/check-dep-release.fish
+++ private_dot_config/private_fish/functions/check-dep-release.fish
@@ -2,7 +2,7 @@ function check-dep-release
     set artifact $argv[1]
 
     for pom in (rg --type-add 'deps:*.deps' -t deps $artifact -l | rg -v materials | cut -d '/' -f 1 | sort | uniq)
-        # git -C $pom status
+        git -C $pom status
         if git -C $pom diff-index --quiet HEAD --
             echo "$pom clean"
         else
