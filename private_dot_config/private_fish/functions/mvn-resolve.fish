0300bfb74f24f8bd525c362d4f8b560e8f1ab814
Mark Derricutt <mark.derricutt@smxemail.com>
2026-08-22 22:12:19 +1200

    Update .config/fish/functions/mvn-resolve-locked.fish Update .config/fish/functions/mvn-resolve.fish

--- private_dot_config/private_fish/functions/mvn-resolve.fish
+++ private_dot_config/private_fish/functions/mvn-resolve.fish
@@ -1,5 +1,5 @@
 function mvn-resolve
-    set RANGERESOLVER 1.2.34
+    set RANGERESOLVER 1.2.35
     if test -f pom.deps
         if test -f release.properties
             echo "Found release.properties, aborting..."
